70c8480739c896d58aeadf18693adbe9fb389ded
Dan Billingham <danbillingham@gmail.com>
2024-11-28 09:21:22 +0100

    chore: update build output to docs directory for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   base: '/SwAdmin/',
   build: {
-    assetsDir: 'assets',
+    outDir: 'docs',
     rollupOptions: {
       output: {
         assetFileNames: (assetInfo) => {
